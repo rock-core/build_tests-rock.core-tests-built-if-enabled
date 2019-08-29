a69769c4ae10459e4eafb009c1d4593910779845
Sylvain <sylvain.joyeux@tidewise.io>
2019-08-29 13:47:44 -0300

    Initial commit

--- test/suite.cpp
+++ test/suite.cpp
@@ -1,5 +1,4 @@
-// Do NOT add anything to this file
-// This header from boost takes ages to compile, so we make sure it is compiled
-// only once (here)
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
+int main()
+{
+    return 0;
+}
